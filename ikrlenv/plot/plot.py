f6aba429cbb4c23debc5fe2bf6b9c6ec54c46b67
Robin Uhrich <robin.uhrich@gmail.com>
2023-12-29 21:48:20 +0100

    added plotting methods

---
+++ ikrlenv/plot/plot.py
@@ -0,0 +1,19 @@
+
+
+from typing import Callable, Tuple
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+import matplotlib.pyplot as plt
+
+def env_plot(plot_func: Callable, fig: Figure = None, ax: Axes = None, *kwargs, ) -> Tuple[Figure, Axes]:
+    def wrapper():
+        if fig is None:
+            fig, ax = plt.subplots
+        ax = plot_func(ax, *kwargs)
+
+    return wrapper
+
+
+
+def plot_base(ax: Axes):
+    
