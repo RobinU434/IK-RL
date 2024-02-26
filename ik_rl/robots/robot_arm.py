464b5e14cccfc8d7858b0329fac0c7d38046aa0d
Robin Uhrich <robin.uhrich@gmail.com>
2024-02-26 18:57:30 +0100

    implemented robot arm

--- ik_rl/robots/robot_arm.py
+++ ik_rl/robots/robot_arm.py
@@ -1,103 +1,273 @@
-from typing import Any, Dict, Tuple
+from abc import ABC, abstractmethod
+import logging
+from typing import Any, Dict, List, Tuple
 import numpy as np
 from numpy import ndarray
 from ik_rl.solver.base_solver import IKSolver
 
-from ik_rl.solver.ccd import ccd
 
+class RobotArm(ABC):
+    def __init__(
+        self, links: ndarray, solver_cls: type = None, solver_args: Dict[str, Any] = {}
+    ) -> None:
+        super().__init__()
 
-class RobotArm:
-    """implementation of a robt arm in 2D space."""
+        assert len(links) > 0
+        self._num_joints = len(links)
+        self._links = links
 
-    def __init__(self, n_joints: int = 1, segment_length: float = 1, solver: type = None, solver_args: Dict[str, Any] = {}) -> None:
-        self._n_joints = n_joints
-        self._segment_length = segment_length
-
-        self._arm_length = self._n_joints * segment_length
+        self._arm_length = sum(links)
         # relative angles
-        self._angles = np.zeros((self._n_joints))
+        self._rel_angles: ndarray
+        self._positions: ndarray
 
-        self._positions = np.zeros(
-            (self._n_joints + 1, 2)
-        )  # 2D, the plus one dim is the origin
-        # init _positions
-        self.set(self._angles)
-        
-        self._solver: IKSolver = solver(self, **solver_args)
+        if solver_cls is None:
+            self._solver = None
+        else:
+            self._solver: IKSolver = solver_cls(self, **solver_args)
 
     def reset(self):
-        self._angles = np.zeros((self._n_joints))
-        self.set(self._angles)
+        self._rel_angles = np.zeros((self._num_joints))
+        self.set_rel_angles(self._rel_angles)
 
-    def set(self, angles: ndarray):
+    def forward(self, angles: ndarray) -> Tuple[ndarray, ndarray]:
+        """do a forward kinematics step on a 2D robot arm
+
+        Args:
+            angles (ndarray): sequence of relative angles for each joint in radians.
+
+        Returns:
+            Tuple[ndarray, ndarray]: translations and rotations in homogeneous transformation for each joint
+        """
+        dim = self._positions.shape[1]
+        positions = np.empty((self._num_joints + 1, dim + 1, dim + 1))
+        positions[0] = np.eye(dim + 1)
+
+        rot_mats = self._rotate_z(angles)
+        translations = np.zeros((self._num_joints, dim))
+        translations[:, 0] = self._links
+        trans_mats = self._translate(translations)
+
+        for idx, (r, t) in enumerate(zip(rot_mats, trans_mats)):
+            positions[idx + 1] = positions[idx] @ r @ t
+
+        translations = positions[:, :-1, -1]
+        rotations = positions[:, :-1, :-1]
+        return translations, rotations
+
+    def backward(self, target: ndarray) -> Tuple[float, bool]:
+        """solve inverse kinematics for a given target
+
+        Args:
+            target (ndarray): _description_
+
+        Returns:
+            Tuple[float, bool]. Error to target, solved
+        """
+        if self._solver is None:
+            logging.error(
+                "Inverse kinematics is not possible because there is no solver."
+            )
+            return
+        dim = self._positions.shape[1]
+
+        # for the angles you have to pass in the relative angles between the joints.
+        # To get the absolute angels you can use the cum sum function
+        # To invert this you can use:
+        # >>> z[1:] -= z[:-1].copy()
+        # add z dimension in target because the solver is build for 3D space
+        # target_embedding = np.zeros(3)
+        # target_embedding[:2] = target
+
+        res_angles = self._solver.solve(target=target)
+        error = self._solver.dist_error
+        solved = self._solver.solved
+
+        # convert relative resulting angles (re_angles) into absolute angles
+        # abs_angles = np.deg2rad(np.array(res_angles).cumsum())
+        self.set_rel_angles(res_angles)
+        
+        return error, solved
+
+    def set_rel_angles(self, angles: ndarray) -> None:
         """applies given action to the arm
 
         Args:
             angles (np.array): array with same length as number of joints
         """
-        self._angles = angles % (2 * np.pi)
+        self._rel_angles = angles  % (2 * np.pi)
+        self._positions, _ = self.forward(angles)
 
-        for idx in range(self._n_joints):
-            origin = self._positions[idx]
-
-            # new position
-            new_pos = np.array([np.cos(self._angles[idx]), np.sin(self._angles[idx])])
-            # new_pos = np.array([np.cos(cumsum[idx]), np.sin(cumsum[idx])])
-            new_pos *= self._segment_length
-
-            # translate position
-            new_pos += origin
-
-            self._positions[idx + 1] = new_pos
-
-    def inv_kin(
-        self, target: ndarray, max_iter: int = 1000, error_min: float = 0.1
-    ) -> Tuple[float, bool, int]:
-        """solve inverse kinematics for a given target
+    def set_abs_angles(self, angles: ndarray) -> None:
+        """_summary_
 
         Args:
-            target (ndarray): _description_
-            max_iter (int): maximum iteration for ccd solver. Defaults to 1000.
-            error_min (float): error threshold for solving the inverse kinematics problem. Defaults to 0.1.
+            angles (ndarray): _description_
 
         Returns:
-            Tuple[float, bool, int]. Error to target, solved, number of iterations needed
+            _type_: _description_
         """
-        # for the angles you have to pass in the relative angles between the joints.
-        # To get the absolute angels you can use the cum sum function
-        # To invert this you can use:
-        # >>> z[1:] -= z[:-1].copy()
-        rel_angles = self.angles.copy()
-        # transform into degrees
-        rel_angles = rel_angles / np.pi * 180
+        rel_angles = angles.copy()
         rel_angles[1:] -= rel_angles[:-1].copy()
+        # abs_angles = np.cumsum(angles, axis=0)
+        self.set_rel_angles(rel_angles)
 
-        res_angles = self._solver.solve(angles=rel_angles, target=target)
-        error = self._solver.dist_error
-        solved = self._solver.solved
+    @staticmethod
+    @abstractmethod
+    def _rotate_z(thetas: ndarray) -> ndarray:
+        raise NotImplementedError
 
-        # convert relative resulting angles (re_angles) into absolute angles
-        abs_angles = np.deg2rad(np.array(res_angles).cumsum())
-        self.set(abs_angles)
-
-        return error, solved
-
-    @property
-    def angles(self):
-        return self._angles
+    @staticmethod
+    @abstractmethod
+    def _translate(thetas: ndarray) -> ndarray:
+        raise NotImplementedError
 
     @property
-    def n_joints(self):
-        return self._n_joints
+    def abs_angles(self) -> ndarray:
+        abs_angles = np.cumsum(self._rel_angles)
+        return abs_angles.copy()
 
     @property
-    def arm_length(self):
+    def rel_angles(self) -> ndarray:
+        return self._rel_angles.copy()
+    
+    @property
+    def n_joints(self) -> int:
+        return self._num_joints
+
+    @property
+    def arm_length(self) -> float:
         return self._arm_length
 
     @property
-    def positions(self):
-        return self._positions
+    def positions(self) -> ndarray:
+        return self._positions.copy()
 
     @property
-    def end_position(self):
+    def end_position(self) -> ndarray:
         return self._positions[-1]
+
+
+class RobotArm2D(RobotArm):
+    """implementation of a robt arm in 2D space."""
+
+    def __init__(
+        self, links: ndarray, solver_cls: type = None, solver_args: Dict[str, Any] = {}
+    ) -> None:
+        super().__init__(links, solver_cls, solver_args)
+        self._rel_angles: ndarray = np.zeros((self._num_joints))
+        # 2D, the plus one dim is the origin
+        self._positions = np.zeros((self._num_joints + 1, 2))
+        # init _positions
+        self.set_rel_angles(self._rel_angles)
+
+    @staticmethod
+    def _rotate_z(thetas: float) -> ndarray:
+        """create 2d rotation matrix over x and y:\\
+        [[math.cos(theta), -math.sin(theta), 0], \\
+         [math.sin(theta), math.cos(theta), 0], \\
+         [0, 0, 1]]
+         
+        Args:
+            thetas (float): angles to rotate around z in radians. Shape (num_angles)
+
+        Returns:
+            ndarray: ration matrix over x and y. Shape (num_angles, 4, 4)
+        """
+        num_thetas = len(thetas)
+        sin = np.sin(thetas)
+        cos = np.cos(thetas)
+
+        first_row = np.stack([cos, -sin])
+        second_row = np.stack([sin, cos])
+
+        rot_mat = np.stack([first_row, second_row])
+        rot_mat = np.moveaxis(rot_mat, -1, 0)
+
+        zeros = np.zeros((num_thetas, 1, 2))
+        identity = np.eye(1)[None].repeat(num_thetas, axis=0)
+
+        rz = np.block([[rot_mat, zeros.swapaxes(-1, -2)], [zeros, identity]])
+        return rz
+
+    def _translate(self, vector: ndarray) -> ndarray:
+        """create translation matrix (num_translation, 4, 4)
+        Example:\\
+        [[1, 0, dx], \\
+         [0, 1, dy], \\
+         [0, 0,  1]]
+
+        Args:
+            vector (ndarray): 1d translation vector. Shape (num_translation, translation_dim)
+
+        Returns:
+            ndarray: translation as augmented matrix. Shape: (num_translation, 4, 4)
+        """
+        num_samples, trans_dim = vector.shape
+        trans = np.eye(3)[None].repeat(num_samples, axis=0)
+        trans[:, :trans_dim, -1] = vector
+        return trans
+
+
+class RobotArm3D(RobotArm):
+    def __init__(
+        self, links: ndarray, solver_cls: type = None, solver_args: Dict[str, Any] = {}
+    ) -> None:
+        super().__init__(links, solver_cls, solver_args)
+        self._rel_angles: ndarray = np.zeros(
+            (self._num_joints, 2)
+        )  # two angles to rotate on
+        # 2D, the plus one dim is the origin
+        self._positions = np.zeros((self._num_joints + 1, 3))
+        # init _positions
+        self.set_rel_angles(self._rel_angles)
+
+        raise NotImplementedError
+
+    @staticmethod
+    def _rotate_z(thetas: float) -> ndarray:
+        """create 2d rotation matrix over x and y:\\
+        [[math.cos(theta), -math.sin(theta), 0, 0], \\
+         [math.sin(theta), math.cos(theta), 0, 0], \\
+         [0, 0, 1, 0], \\
+         [0, 0, 0, 1],]
+
+        Args:
+            thetas (float): angles to rotate around z in radians. Shape (num_angles)
+
+        Returns:
+            ndarray: ration matrix over x and y. Shape (num_angles, 4, 4)
+        """
+        num_thetas = len(thetas)
+        sin = np.sin(thetas)
+        cos = np.cos(thetas)
+
+        first_row = np.stack([cos, -sin])
+        second_row = np.stack([sin, cos])
+
+        rot_mat = np.stack([first_row, second_row])
+        #rot_mat = np.moveaxis(rot_mat, -1, 0)   
+
+        zeros = np.zeros((num_thetas, 2, 2))
+        identity = np.eye(2)[None].repeat(num_thetas, axis=0)
+        rz = np.block([[rot_mat, zeros], [zeros, identity]])
+        return rz
+
+    def _translate(self, vector: ndarray) -> ndarray:
+        """create translation matrix (num_translation, 4, 4)
+        Example:\\
+        [[1, 0, 0, dx], \\
+         [0, 1, 0, dy], \\
+         [0, 0, 1, dz], \\
+         [0, 0, 0,  1]]
+
+        Args:
+            vector (ndarray): 1d translation vector. Shape (num_translation, translation_dim)
+
+        Returns:
+            ndarray: translation as augmented matrix. Shape: (num_translation, 4, 4)
+        """
+        num_samples, trans_dim = vector.shape
+        trans = np.eye(4)[None].repeat(num_samples, axis=0)
+        trans[:, :trans_dim, -1] = vector
+        return trans
