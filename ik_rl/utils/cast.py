50caf0f62005470b716183bd08e0f1a301052e86
Robin Uhrich <robin.uhrich@gmail.com>
2024-03-07 12:25:03 +0100

    added tensor_to_array method

---
+++ ik_rl/utils/cast.py
@@ -0,0 +1,19 @@
+
+
+from numpy import ndarray
+from torch import Tensor
+
+
+def tensor_to_ndarray(tensor: Tensor | ndarray) -> ndarray:
+    """assumes the input is either a ndarray or a tensor and returns a ndarray
+
+    Args:
+        tensor (Tensor | ndarray): input array
+
+    Returns:
+        ndarray: nd array
+    """
+    if isinstance(tensor, Tensor):
+        # detach if the action tensor requires grad = True
+        return tensor.detach().numpy()
+    return tensor
